a57d95eca44d1af496a60fa0be12627247087d5a
lohitha <lohitha.99630@gmail.com>
2025-09-26 19:01:44 +0530

    baseurl chnages to deploy url

--- src/api/baseURL.ts
+++ src/api/baseURL.ts
@@ -1,2 +1,3 @@
-export const baseUrl = "https://services-platform-backend.onrender.com";
+export const baseUrl = "https://backend-750279619095.asia-south1.run.app/";
+// export const baseUrl = "https://services-platform-backend.onrender.com";
 // export const baseUrl = "http://localhost:5000";
